37b56074751f9df405e5c48f1d27fe6003afdb9d
TUdayKiranReddy <53467166+TUdayKiranReddy@users.noreply.github.com>
2020-01-25 11:14:40 +0530

    Found prime implecants without arranging it in ascending order.

--- function.py
+++ function.py
@@ -1,4 +1,10 @@
 ####Remenber to kepp all zeroes at n+1 term in string######
+
+def init(A):
+	for i in range(len(A)):
+		A[i] = A[i] + '0'
+	return A
+
 def noof(A):
 	q = 0
 	for f in range(0,len(A)-1):
@@ -6,15 +12,15 @@ def noof(A):
 			q+=1
 	return q
 	
-def order(B):
-	for i in range(1,len(B)-1):
-		key = noof(B[i])
-		j = i -1	
-		while j >=0 and key < noof(B[j]):
-			B[j+1] = B[j]
-			j -=1
-		B[j+1] = B[i]
-	return B
+# ~ def order(B):
+	# ~ for i in range(1,len(B)):
+		# ~ key = noof(B[i])
+		# ~ j = i -1	
+		# ~ while j >=0 and key < noof(B[j]):
+			# ~ B[j+1] = B[j]
+			# ~ j -=1
+		# ~ B[j+1] = key
+	# ~ return B
 
 def common(A,B):
 	count = 0
@@ -41,9 +47,33 @@ def status(A,B):
 
 def nxtbl(A):   ####m ------ no of tures for output
 	C =[]
-	for i in range(m-1):
-		for j in range(i,m):
+	for i in range(len(A)-1):
+		for j in range(i,len(A)):
 			if(common(A[i],A[j])):
 				C.append(common(A[i],A[j]))
 			A[i],A[j] = status(A[i],A[j])
 	return C
+
+def primertr(A):
+	C = []
+	for i in range(len(A)):
+		for l in A[i]:
+			if (l[-1] == '0'):
+				C.append(l)
+	if(nxtbl(C)):
+		for i in range(len(nxtbl(C))):
+			nxtbl(C)[i] = nxtbl(C)[i][:-1]
+		return nxtbl(C)
+	else:
+		for i in range(len(C)):
+			C[i] = C[i][:-1]
+		return C
+		
+def endchk(A):
+	for i in range(len(A)):
+		if(A[i][-1] == '0'):
+			i = 0
+	if(i == 0):
+		return True
+	else:
+		return False
--- main.py
+++ main.py
@@ -1,5 +1,24 @@
 from function import *
+import time
 
-A = ['0010','1001','1110','1111']
-print(len(A[0]))
-print(order(A))
+start_time = time.time()
+C = ['0000','0011','0101','0110','0111','1010','1100','1101','0010','1001','1111']
+
+A = []
+C = init(C)
+m = len(C)
+
+A.append(C)
+A.append(nxtbl(C))
+#print(A[0])
+
+for i in range(1,m-1):
+	if(A[i] !=[]):
+		A.append(nxtbl(A[i]))
+		#print(A[i])
+	else:
+		break
+	
+#print(primertr(A))
+
+print("--- %s seconds ---" % (time.time() - start_time))
