1a27fbd98789099e67c52e9e4a0af347f6962f5b
TUdayKiranReddy <53467166+TUdayKiranReddy@users.noreply.github.com>
2020-02-01 21:40:39 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,28 +1,15 @@
 from function import *
-import time
-import string
 
-start_time = time.time()
-C = ['0000','0011','0101','0110','0111','1010','1100','1101','0010','1001','1111']
+Minimised_Prime_Implecants,Prime_Implecants,Inputs,No_of_variables,time_taken = main("ssd.txt")
+W =[]
+for a in Prime_Implecants:
+	W.append((expressiongnr(a)))
+print("Prime Implecants",W)
 
-A = []
-C = init(C)
-#print(order(C))
-m = len(C)
-C = order(C)
-A.append(C)
-A.append(nxtbl(C))
-#print(A[0])
+W =[]
+for a in Prime_Implecants:
+	W.append((expressiongnr(a)))
+print("Prime Implecants",W)
+print("Final Expression is ",expr(Minimised_Prime_Implecants))
 
-for i in range(1,m-1):
-	if(A[i] !=[]):
-		A.append(nxtbl(A[i]))
-		#print(A[i])
-	else:
-		break
-A = A[:-1]
-
-print(primertr(A))
-for a in primertr(A):
-	print(expressiongnr(a))
-print("--- %s seconds ---" % (time.time() - start_time))
+table(Prime_Implecants,Inputs,create_chart_save(Prime_Implecants,Inputs))
